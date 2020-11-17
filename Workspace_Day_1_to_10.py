69490bcf91ff18642e2b8ddc5cef8c1016280907
Rebecca-Ingram <beckyingramrva@gmail.com>
2020-11-17 17:59:01 +0100

    day 5 practice exercise

--- Workspace_Day_1_to_10.py
+++ Workspace_Day_1_to_10.py
@@ -275,3 +275,26 @@ elif computer_choice > user_choice:
     print("Computer wins!")
 elif computer_choice == user_choice:
     print("It's a draw")
+
+# Day 5
+# Practice Exercise - Average student height in a class
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights = int(student_heights[n])
+# print(student_heights)
+
+total_height = 0
+for height in student_heights:
+    total_height += height
+print(total_height)
+
+number_of_students = 0
+for student in student_heights:
+    number_of_students += 1
+print(number_of_students)
+
+avg_height = round(total_height/number_of_students)
+print(avg_height)
+
+
+# Password generator
