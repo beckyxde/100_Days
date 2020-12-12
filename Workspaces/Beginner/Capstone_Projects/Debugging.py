3d4ce8a3c5a37e43ceca9dc02947da47b0c250e1
Rebecca-Ingram <41595749+beckyxde@users.noreply.github.com>
2020-12-12 23:09:52 +0100

    Day 13

---
+++ Workspaces/Beginner/Capstone_Projects/Debugging.py
@@ -0,0 +1,12 @@
+# Debugging FizzBuzz
+
+for number in range(1, 100):
+    # print(f"Currently on number {number}")
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
