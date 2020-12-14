34cec90d529fda859c0d601411b450729de86dcc
Rebecca-Ingram <41595749+beckyxde@users.noreply.github.com>
2020-12-14 22:30:34 +0100

    Day 13 - more debugging exercises

--- Workspaces/Beginner/Capstone_Projects/Debugging.py
+++ Workspaces/Beginner/Capstone_Projects/Debugging.py
@@ -1,5 +1,28 @@
-# Debugging FizzBuzz
+# Debugging Odd or Even
+number = int(input("Which number do you want to check? "))
 
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
+
+# Debugging LeapYear
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
+
+
+# Debugging FizzBuzz
 for number in range(1, 100):
     # print(f"Currently on number {number}")
     if number % 3 == 0 and number % 5 == 0:
