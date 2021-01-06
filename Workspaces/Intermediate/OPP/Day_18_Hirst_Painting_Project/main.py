b3774c68d3edc25ed76c6c762deeae2efb9c171e
Rebecca-Ingram <41595749+beckyxde@users.noreply.github.com>
2021-01-06 23:47:41 +0100

    Hirst Painting Project edited & reference image added

--- Workspaces/Intermediate/OPP/Day_18_Hirst_Painting_Project/main.py
+++ Workspaces/Intermediate/OPP/Day_18_Hirst_Painting_Project/main.py
@@ -1,19 +1,34 @@
+import colorgram
 import turtle as turtle_module
 import random
+
+'''extract color palette from image using colorgram'''
+# colors = colorgram.extract('hirst.jpg', 10)
+# rgb_colors = []
+
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
 
 turtle_module.colormode(255)
 tim = turtle_module.Turtle()
 tim.speed("fastest")
 tim.penup()
 tim.hideturtle()
-color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
+colors = [(131, 166, 205), (222, 148, 106), (31, 42, 61), (199, 134, 147), (165, 59, 48),(140, 184, 162)]
+#the closer the colors are to '255' the more likely they're a shade of white, aka the background color
 tim.setheading(225)
 tim.forward(300)
 tim.setheading(0)
 number_of_dots = 100
 
+# tim.dot(random.choice(colors))
+
 for dot_count in range(1, number_of_dots + 1):
-    tim.dot(20, random.choice(color_list))
+    tim.dot(20, random.choice(colors))
     tim.forward(50)
 
     if dot_count % 10 == 0:
@@ -23,13 +38,7 @@ for dot_count in range(1, number_of_dots + 1):
         tim.forward(500)
         tim.setheading(0)
 
-
-
-
-
-
-
-
+# print(rgb_colors)
 
 screen = turtle_module.Screen()
 screen.exitonclick()
