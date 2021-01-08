babab31e32f03b217705a613cd3508f78fb5e7ba
Rebecca-Ingram <41595749+beckyxde@users.noreply.github.com>
2021-01-08 20:49:24 +0100

    Etch-A-Sketch Exercise

---
+++ Workspaces/Intermediate/OPP/Day_19_Turtle_Coordinate_System_Quiz/Etch-A-Sketch.py
@@ -0,0 +1,30 @@
+#Exercise using event listeners
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(100)
+
+def move_bacwards():
+    tim.backward(100)
+
+def move_cclockwise():
+    tim.left(100)
+
+def move_clockwise():
+    tim.right(100)
+
+def clear_screen():
+    tim.clear()
+    tim.home()
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_bacwards, "s")
+screen.onkey(move_cclockwise, "a")
+screen.onkey(move_clockwise, "d")
+screen.onkey(clear_screen, "c")
+screen.exitonclick()
